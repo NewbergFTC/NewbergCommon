b265de88d67417b1cd6553a5a9a916d0191ffeff
PavoReal <Garrison.peacock@gmail.com>
2017-02-02 15:31:41 -0800

    Added mm to inch conversions

--- src/main/java/us/or/k12/newberg/newbergcommon/math/MathUtil.java
+++ src/main/java/us/or/k12/newberg/newbergcommon/math/MathUtil.java
@@ -6,6 +6,9 @@ package us.or.k12.newberg.newbergcommon.math;
 
 public class MathUtil
 {
+    public static final float MM_PER_INCH = 25.4f;
+    public static final float MM_FTC_FIELD_WIDTH = (12.0f * 12.0f - 2.0f) * MM_PER_INCH; 
+
     private static final double EPSILON = 0.000001; // A very tiny number (10e-6)
 
     public static boolean FuzzyEquals(double value1, double value2)
